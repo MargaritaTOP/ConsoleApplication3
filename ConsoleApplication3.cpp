6092463a7774183f7b19bcbc2f8baf115e4bb818
User-7 <User-7@USER-7>
2024-05-12 19:37:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+// рассмотрим особенности работы со структурами
+
+//Программа спрашивает у пользователя данные.Пользователь вводит данные.Программа спрашивает у пользователя маску.К данным применяется битовая операция xor ^с маской и программа выводит на экран зашифрованные данные.Также вывести дешифровку.
+
+//Морской бой
+
+#include <iostream>
+#include <Windows.h>
+#include <io.h>
+#include <minwinbase.h>
+#include <dos.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    cout << "Hello world" << endl;
+        
+   return 0;
+}
+
+
+    
+
+
+   
